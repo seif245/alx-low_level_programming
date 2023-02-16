c9a2351e1c207852a6d61ea6147866fe86f027ed
seif <seif@alx.me>
2023-02-16 14:46:16 -0800

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
-#include<unistd.c>
+#include<unistd.c
+#incude<stdio.h>>
 /** main - prints and that piece of art is useful" - Dora Korpar, 2015-10-1
- * Return: Always 19
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write((2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	fput(( "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
