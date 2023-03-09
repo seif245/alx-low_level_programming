7778b6dcb1b58d5c810791cdca36e2935dc7d09f
seif <seif@alx.me>
2023-03-09 08:15:23 -0800

    first

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -14,18 +14,18 @@ void _puts_recursion(char *s)
 
 {
 
-        if (*s)
+	if (*s)
 
-        {
+	{
 
-                _putchar(*s);
+		_putchar(*s);
 
-                _puts_recursion(s + 1);
+		_puts_recursion(s + 1);
 
-        }
+	}
 
-        else
+	else
 
-                _putchar('\n');
+	_putchar('\n');
 
 }
